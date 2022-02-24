95bd63f565344f10a79beb428b6eed765a0b6905
Yaroslav Starchenkov <starchenkov.ya@gmail.com>
2022-02-25 03:38:07 +0700

    feat: сформировать app.js

---
+++ app.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const { errors } = require('celebrate');
+const cors = require('cors');
+
+const errorHandler = require('./utils/middlewares/handlers/error-handler');
+
+const routs = require('./routes');
+
+const { PORT, DB_ADDRESS } = require('./utils/configs/app-config');
+const corsConfig = require('./utils/configs/cors-config');
+
+const app = express();
+
+mongoose.connect(DB_ADDRESS, {
+  useNewUrlParser: true,
+});
+
+app.use(cors(corsConfig));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(routs);
+
+app.use(errors());
+app.use(errorHandler);
+
+app.listen(PORT);
---
+++ utils/configs/cors-config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  origin: 'https://moviesexplorer.nomoredomains.work',
+  credentials: true,
+};
